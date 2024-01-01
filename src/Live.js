e972b61fd31b732bc67ef788a1e21b48ccd20117
Thomas Kinsella <thomasjkinsella4@gmail.com>
2024-01-01 13:49:24 -0500

    showupdate

--- src/Live.js
+++ src/Live.js
@@ -3,7 +3,7 @@ import React from "react";
 function Live() {
     return (
         <div id="live-dates">
-        <h1 className='live-shows-venue'><div className="date"></div>SHOW DATES TBA</h1>
+        <h1 className='live-shows-venue'><div className="date"></div>01. 27. 24 - THE DELANCEY, NYC - 9PM</h1>
       </div> 
     )
 }
