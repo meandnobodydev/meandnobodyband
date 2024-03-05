a7d9ddfccdbb9631395da9299733851b9129b8d3
Thomas Kinsella <thomasjkinsella4@gmail.com>
2024-03-04 21:48:33 -0500

    update shows

--- src/Live.js
+++ src/Live.js
@@ -3,8 +3,8 @@ import React from "react";
 function Live() {
     return (
         <div id="live-dates">
-        <h1 className='live-shows-venue'><div className="date"></div>02. 15. 24 - NUBLU, NYC - 7PM</h1>
         <h1 className='live-shows-venue'><div className="date"></div>03. 10. 24 - THE BOWERY ELECTRIC, NYC - 7PM</h1>
+        <h1 className='live-shows-venue'><div className="date"></div>04. 26. 24 - THE BOWERY ELECTRIC, NYC - 7PM</h1>
       </div> 
     )
 }
