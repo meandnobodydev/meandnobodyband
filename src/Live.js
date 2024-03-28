90588cb00a66b07b7caedb6b0ba1776d57927539
Thomas Kinsella <thomasjkinsella4@gmail.com>
2024-03-27 20:21:30 -0400

    update  dates

--- src/Live.js
+++ src/Live.js
@@ -3,8 +3,7 @@ import React from "react";
 function Live() {
     return (
         <div id="live-dates">
-        <h1 className='live-shows-venue'><div className="date"></div>03. 10. 24 - THE BOWERY ELECTRIC, NYC - 7PM - <a className="tix-link" href="https://www.ticketweb.com/event/me-nobody-with-spicy-the-bowery-electric-tickets/13536473?pl=boweryelectric&REFID=clientsitewp" target="_blank">TICKETS</a></h1>
-        <h1 className='live-shows-venue'><div className="date"></div>04. 26. 24 - THE BOWERY ELECTRIC, NYC - 7PM</h1>
+        <h1 className='live-shows-venue'><div className="date"></div>04. 26. 24 - PINK FROG CAFE, BK - 8:30PM</h1>
       </div> 
     )
 }
