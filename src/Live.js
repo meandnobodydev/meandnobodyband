afabd6f738b395d00e0606360eaa9cbca8a7e8d3
Thomas Kinsella <thomasjkinsella4@gmail.com>
2024-06-20 17:32:15 -0400

    show update

--- src/Live.js
+++ src/Live.js
@@ -3,7 +3,7 @@ import React from "react";
 function Live() {
     return (
         <div id="live-dates">
-        <h1 className='live-shows-venue'><div className="date"></div>04. 26. 24 - PINK FROG CAFE, BK - 8:30PM</h1>
+        <h1 className='live-shows-venue'><div className="date"></div>SHOW DATES TBA</h1>
       </div> 
     )
 }
